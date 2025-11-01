c7314c62450ed9be935be8c4827ebe1a466dc03a
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-11-01 12:34:54 -0300

    fix: Animations and movimentation

--- player.gd
+++ player.gd
@@ -2,40 +2,50 @@ extends CharacterBody2D
 
 var directionLast : Vector2 = Vector2.ZERO
 const SPEED = 300.0
-@onready var sprite := $AnimationPlayer
+@onready var sprite : AnimatedSprite2D = $sprite
 
 func _physics_process(delta: float) -> void:
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction : Vector2 = Vector2.ZERO
 	
 	var moving : bool = false
 	
+	sprite.flip_h = false
+	
 	if Input.is_action_pressed("Down"):
 		direction.y = 1
 		moving = true
-		sprite.play("RunDown")
-		
+		sprite.play("runDown")
 	
-	if Input.is_action_pressed("Up"):
+	elif Input.is_action_pressed("Up"):
 		direction.y = -1
 		moving = true
-		sprite.play("RunUp")
-	
+		sprite.play("runUp")
+		
 	if Input.is_action_pressed("Right"):
 		direction.x = 1
 		moving = true
+		sprite.play("runX")
 	
-	if Input.is_action_pressed("Left"):
+	elif Input.is_action_pressed("Left"):
 		direction.x = -1
 		moving = true
+		sprite.play("runX")
+		sprite.flip_h = true
 	
 	if moving:
 		velocity = direction * SPEED
+		directionLast = direction
 	else:
 		velocity = Vector2.ZERO
-	
-	
+		if directionLast.x == 1:
+			sprite.play("idleX")
+		elif directionLast.x == -1:
+			sprite.play("idleX")
+			sprite.flip_h = true
+		elif directionLast.y == 1:
+			sprite.play("idleDown")
+		else:
+			sprite.play("idleUp")
+		
 	
 	move_and_slide()
